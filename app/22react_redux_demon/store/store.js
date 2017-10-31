488aacf941668b7254c1d61f52c922fe045898be
王园龙 <530197181@qq.com>
2017-10-31 15:50:54 +0800

    中间件 middleWare

--- app/22react_redux_demon/store/store.js
+++ app/22react_redux_demon/store/store.js
@@ -7,8 +7,51 @@ const rootReducer = combineReducers({
     // tab,
     // user,
 });
+/**
+ * 让你可以发起一个函数来替代 action。
+ * 这个函数接收 `dispatch` 和 `getState` 作为参数。
+ *
+ * 对于（根据 `getState()` 的情况）提前退出，或者异步控制流（ `dispatch()` 一些其他东西）来说，这非常有用。
+ *
+ * `dispatch` 会返回被发起函数的返回值。
+ */
+const thunk = store => next => action =>
+    typeof action === 'function' ?
+        action(store.dispatch, store.getState) :
+        next(action)
 
-let store = createStore(rootReducer);
+
+const logger = store => next => action => {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    return result
+}
+
+const crashReporter = store => next => action => {
+    try {
+        return next(action)
+    } catch (err) {
+        console.error('Caught an exception!', err)
+        Raven.captureException(err, {
+            extra: {
+                action,
+                state: store.getState()
+            }
+        });
+        throw err
+    }
+};
+// applyMiddleware 接收 createStore()
+// 并返回一个包含兼容 API 的函数。
+let createStoreWithMiddleware = applyMiddleware(
+    logger,
+    thunk,
+    crashReporter
+)(createStore);
+let store = createStoreWithMiddleware(rootReducer);
+
+// let store = createStore(rootReducer);
 export const getStore = () => {
     return store;
 };
